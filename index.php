2ce8653774dbfadec9ca05868514fdb76d58f84d
Gregory Post <gregory.post@asu.edu>
2024-05-13 13:40:49 -0700

    Add index.php

---
+++ index.php
@@ -0,0 +1,53 @@
+<?php
+include_once('config/symbini.php');
+if($LANG_TAG == 'en' || !file_exists($SERVER_ROOT.'/content/lang/index.'.$LANG_TAG.'.php')) include_once($SERVER_ROOT.'/content/lang/index.en.php');
+else include_once($SERVER_ROOT.'/content/lang/index.'.$LANG_TAG.'.php');
+header('Content-Type: text/html; charset=' . $CHARSET);
+?>
+<html>
+<head>
+	<title><?php echo $DEFAULT_TITLE; ?> Home</title>
+	<?php
+	include_once($SERVER_ROOT . '/includes/head.php');
+	include_once($SERVER_ROOT . '/includes/googleanalytics.php');
+	?>
+</head>
+<body>
+	<?php
+	include($SERVER_ROOT . '/includes/header.php');
+	?>
+	<div class="navpath"></div>
+	<div id="innertext">
+		<?php
+		if($LANG_TAG == 'es'){
+			?>
+			<div>
+				<h1 class="headline">Bienvenidos</h1>
+				<p>Este portal de datos se ha establecido para promover la colaboración... Reemplazar con texto introductorio en inglés</p>
+			</div>
+			<?php
+		}
+		elseif($LANG_TAG == 'fr'){
+			?>
+			<div>
+				<h1 class="headline">Bienvenue</h1>
+				<p>Ce portail de données a été créé pour promouvoir la collaboration... Remplacer par le texte d'introduction en anglais</p>
+			</div>
+			<?php
+		}
+		else{
+			//Default Language
+			?>
+			<div>
+				<h1>Welcome</h1>
+				<p>The International Taphonomy Reference Collection Portal provides specimen-based information on bone modification and other features of modern and fossil bones and bone assemblages.   It is designed to help paleobiologists, anthropologists, archaeologists, ecologists, and forensic scientists share information that will help them interpret various types of taphonomic evidence. </p>
+			</div>
+			<?php
+		}
+		?>
+	</div>
+	<?php
+	include($SERVER_ROOT . '/includes/footer.php');
+	?>
+</body>
+</html>
